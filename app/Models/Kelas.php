7dadec8e0b733b63df4a80d4aab8459c3079516c
Dhimas Dzulfajri <ddzulfajri@gmail.com>
2025-10-03 00:55:08 +0700

    Selesai Langkah Praktikum Modul 4

--- app/Models/Kelas.php
+++ app/Models/Kelas.php
@@ -15,4 +15,8 @@ class Kelas extends Model
     {
         return $this->hasMany(UserModel::class, 'kelas-id');
     }
+
+    public function getKelas(){
+        return $this->all();
+    }
 }
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use GuzzleHttp\Psr7\Request;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -15,5 +16,13 @@ class UserModel extends Model
     public function user()
     {
         return $this->belongsTo(UserModel::class, 'kelas-id');
+    }
+
+    public function getUser()
+    {
+
+        return $this->join('kelas', 'kelas.id', '=', 'user.kelas_id')
+                    ->select('user.*', 'kelas.nama_kelas as nama_kelas')
+                    ->get();
     }
 }
